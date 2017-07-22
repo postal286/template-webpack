81ae9d262a5476f07263316b1826864d13be89f9
ApostoL <ApostoL>
2017-07-23 03:04:27 +0600

    updated dependencies

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<h1 style={textAlign: 'center'}>Webpack Template made by A. Kovalev :)</h1>
+				<h1>Webpack Template made by A. Kovalev :)</h1>
 			</div>
 		);
 	}
